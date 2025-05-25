f9420b2f09fdc1c482115a8bb7b981ce6db411cf
Nikleolusso <nikalbrecht@gmx.net>
2025-05-25 15:15:20 +0200

    Update game.c
    functions for shuffeling, dealing

--- game.c
+++ game.c
@@ -1,1 +1,32 @@
 //For Functions
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "game.h"
+
+//Swap two cards (for shuffle function)
+void swap(const char** a, const char** b) {
+    const char* temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Fisher-Yates shuffle
+void shuffleDeck(const char* deck[], int DECK_SIZE) {
+    for (int i = DECK_SIZE - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        swap(&deck[i], &deck[j]);
+    }
+
+}
+
+//Deal cards
+void dealCards(Player players[], const char* deck[], int numPlayers, int cardsPerPlayer) {
+    int index = 0;
+    for(int i, i < cardsPerPlayer, i++) {
+        for(int j, j < numPlayers, j++) {
+        players[j].hand[i] = deck[index++];
+        players.cardCount++;
+        }
+    }
+} 
