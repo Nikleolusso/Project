bfe2a53e4979f0043edf699feffc956f936c3063
Nikleolusso <nikalbrecht@gmx.net>
2025-05-25 15:13:24 +0200

    Update main.c
    added card deck, shuffling and dealing mechanisms

--- main.c
+++ main.c
@@ -1,1 +1,45 @@
 //For Player interactions
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "game.h"
+
+int main() {
+
+Player players[NUM_PLAYERS];
+const int CARDS_PER_PLAYER 12;
+const int NUM_PLAYERS 4;
+    
+const char* deck[] = {
+    // 11 Taco cards
+    "Taco", "Taco", "Taco", "Taco", "Taco", "Taco", "Taco", "Taco", "Taco", "Taco", "Taco",
+
+    // 11 Cat cards
+    "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+
+    // 11 Goat cards
+    "Goat", "Goat", "Goat", "Goat", "Goat", "Goat", "Goat", "Goat", "Goat", "Goat", "Goat",
+
+    // 11 Cheese cards
+    "Cheese", "Cheese", "Cheese", "Cheese", "Cheese", "Cheese", "Cheese", "Cheese", "Cheese", "Cheese", "Cheese",
+
+    // 11 Pizza cards
+    "Pizza", "Pizza", "Pizza", "Pizza", "Pizza", "Pizza", "Pizza", "Pizza", "Pizza", "Pizza", "Pizza",
+
+    // 3 Gorilla cards
+    "Gorilla", "Gorilla", "Gorilla",
+
+    // 3 Groundhog cards
+    "Groundhog", "Groundhog", "Groundhog",
+
+    // 3 Narwhal cards
+    "Narwhal", "Narwhal", "Narwhal"
+};
+
+//Game preparation (shuffleing and dealing cards)
+srand((unsigned int)time(NULL)); //create seed for shuffle function
+shuffleDeck(deck, DECK_SIZE); //call shuffle function
+dealCards(players, deck, NUM_PLAYERS, CARDS_PER_PLAYER);
+
+return 0;
+}
